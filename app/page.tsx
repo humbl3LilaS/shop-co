f4bf094af0ed5a1ad14cd34ad9e45efac1045b42
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-20 14:04:14 +0630

    add: hero section for all screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/hero";
+
 export default function Home() {
     return (
-        <div className="text-3xl">
-            <h1 className={"font-title"}>Hello</h1>
-        </div>
+        <main>
+            <Hero/>
+        </main>
     );
 }
