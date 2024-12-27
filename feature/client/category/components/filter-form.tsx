c3b7ae650a90745963f00590816b41bde26d897f
humbl3LilaS <sabishinekobebe@gmail.com>
2024-12-27 14:11:02 +0630

    bug-fix: resetting filter in filter form doesn't reset the form state even though searchParams are resetted.

--- feature/client/category/components/filter-form.tsx
+++ feature/client/category/components/filter-form.tsx
@@ -1,188 +1,184 @@
-"use client"
-import {SubmitHandler, useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {FilterFormDefaultValues, FilterFormSchema, FilterFormSchemaType} from "@/validation/client-schema";
-import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {SIZES, TYPES} from "@/constants/constants";
-import {Label} from "@/components/ui/label";
-import {Checkbox} from "@/components/ui/checkbox";
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FilterFormDefaultValues, FilterFormSchema, FilterFormSchemaType } from "@/validation/client-schema";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { SIZES, TYPES } from "@/constants/constants";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
 import PriceRangeSelector from "@/components/share/price-range-selector";
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import AccordionField from "@/feature/client/category/components/accordion-field";
-import {arrayToSlug} from "@/lib/utils";
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
-import {useFilterSheet} from "@/feature/client/category/hooks/use-filter-sheet";
+import { arrayToSlug } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useFilterSheet } from "@/feature/client/category/hooks/use-filter-sheet";
 
 function fieldArrayOnChange<T>(value: T, array: T[], callback: (value: T[]) => void) {
-    if (array.includes(value)) {
-        const newArray = array.filter(item => item !== value)
-        callback([...newArray]);
-    } else {
-        callback([...array, value]);
-    }
+	if (array.includes(value)) {
+		const newArray = array.filter((item) => item !== value);
+		callback([...newArray]);
+	} else {
+		callback([...array, value]);
+	}
 }
 
 type FilterFormProps = {
-    defaultValues?: FilterFormSchemaType
-}
+	defaultValues?: FilterFormSchemaType;
+};
 
-const FilterForm = ({defaultValues}: FilterFormProps) => {
+const FilterForm = ({ defaultValues }: FilterFormProps) => {
+	console.log(defaultValues);
+	const form = useForm<FilterFormSchemaType>({
+		resolver: zodResolver(FilterFormSchema),
+		mode: "onChange",
+		defaultValues: { ...FilterFormDefaultValues, ...defaultValues },
+	});
 
-    const form = useForm<FilterFormSchemaType>({
-        resolver: zodResolver(FilterFormSchema),
-        mode: "onChange",
-        defaultValues: {...FilterFormDefaultValues, ...defaultValues},
-    });
+	const router = useRouter();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+	const setOpen = useFilterSheet((state) => state.setOpen);
 
-    const router = useRouter();
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-    const setOpen = useFilterSheet(state => state.setOpen);
+	const onSubmit: SubmitHandler<FilterFormSchemaType> = (values) => {
+		const processedValues = {
+			types: arrayToSlug(values.types),
+			sizes: arrayToSlug(values.sizes),
+			min: values.priceRange[0],
+			max: values.priceRange[1],
+		};
 
-    const onSubmit: SubmitHandler<FilterFormSchemaType> = (values) => {
+		// only get the page.tsx params from the search params others are provided via defaultValues prop
+		const params = new URLSearchParams(searchParams.get("page") ?? "");
 
-        const processedValues = {
-            types: arrayToSlug(values.types),
-            sizes: arrayToSlug(values.sizes),
-            min: values.priceRange[0],
-            max: values.priceRange[1]
-        }
+		// set the searchParams object if the values[key] is not "" or undefined
+		(Object.keys(processedValues) as (keyof typeof processedValues)[]).forEach((key) => {
+			if (processedValues[key]) {
+				params.set(key, processedValues[key].toString());
+			}
+		});
 
-        // only get the page.tsx params from the search params others are provided via defaultValues prop
-        const params = new URLSearchParams(searchParams.get("page") ?? "");
+		//  Toggle the filter-sheet's state
+		setOpen(false);
 
-        // set the searchParams object if the values[key] is not "" or undefined
-        (Object.keys(processedValues) as (keyof typeof processedValues)[])
-            .forEach(key => {
-                if (processedValues[key]) {
-                    params.set(key, processedValues[key].toString());
-                }
-            })
+		// push the searchParam to the current route
+		router.push(`${pathname}?${params.toString()}`);
+	};
 
-        //  Toggle the filter-sheet's state
-        setOpen(false);
+	const resetFilterHandler = () => {
+		form.reset();
+		router.push(pathname);
+	};
 
-        // push the searchParam to the current route
-        router.push(`${pathname}?${params.toString()}`);
-    }
+	return (
+		<div className={"mt-6"}>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<FormField
+						name={"types"}
+						control={form.control}
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className={"sr-only"}>Product Type</FormLabel>
+								<FormControl>
+									<div className={"flex flex-col gap-y-4"}>
+										{TYPES.map((type, idx) => (
+											<div
+												key={type + idx}
+												className={"flex items-center justify-between"}>
+												<Label
+													htmlFor={type}
+													className={"capitalize text-black/60"}>
+													{type}
+												</Label>
+												<Checkbox
+													id={type}
+													className={"size-5"}
+													onCheckedChange={() => {
+														fieldArrayOnChange(type, field.value, field.onChange);
+													}}
+													checked={field.value.includes(type)}
+												/>
+											</div>
+										))}
+									</div>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-    const resetFilterHandler = () => {
-        router.push(pathname);
-    }
+					<hr className={"my-6"} />
 
-    return (
-        <div className={"mt-6"}>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
+					<AccordionField title={"Price"}>
+						<FormField
+							name={"priceRange"}
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<PriceRangeSelector
+											value={field.value}
+											onChange={field.onChange}
+											step={50}
+											min={0}
+											max={400}
+										/>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+					</AccordionField>
 
-                    <FormField
-                        name={"types"}
-                        control={form.control}
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel className={"sr-only"}>
-                                    Product Type
-                                </FormLabel>
-                                <FormControl>
-                                    <div className={"flex flex-col gap-y-4"}>
-                                        {
-                                            TYPES.map((type, idx) =>
-                                                <div key={type + idx}
-                                                     className={"flex items-center justify-between"}>
-                                                    <Label htmlFor={type} className={"capitalize text-black/60"}>
-                                                        {type}
-                                                    </Label>
-                                                    <Checkbox
-                                                        id={type}
-                                                        className={"size-5"}
-                                                        onCheckedChange={() => {
-                                                            fieldArrayOnChange(type, field.value, field.onChange)
-                                                        }}
-                                                        checked={field.value.includes(type)}
-                                                    />
-                                                </div>
-                                            )
-                                        }
-                                    </div>
-                                </FormControl>
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+					<AccordionField title={"Size"}>
+						<FormField
+							name={"sizes"}
+							control={form.control}
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<div className={"flex flex-col gap-y-4"}>
+											{SIZES.map((size, idx) => (
+												<div
+													key={size + idx}
+													className={"flex items-center justify-between"}>
+													<Label
+														htmlFor={size}
+														className={"capitalize text-black/60"}>
+														{size}
+													</Label>
+													<Checkbox
+														id={size}
+														className={"size-5"}
+														checked={field.value.includes(size)}
+														onCheckedChange={() => {
+															fieldArrayOnChange(size, field.value, field.onChange);
+														}}
+													/>
+												</div>
+											))}
+										</div>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</AccordionField>
 
-                    <hr className={"my-6"}/>
-
-                    <AccordionField title={"Price"}>
-                        <FormField
-                            name={"priceRange"}
-                            control={form.control}
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormControl>
-                                        <PriceRangeSelector
-                                            value={field.value}
-                                            onChange={field.onChange}
-                                            step={50}
-                                            min={0}
-                                            max={400}
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                    </AccordionField>
-
-                    <AccordionField title={"Size"}>
-                        <FormField
-                            name={"sizes"}
-                            control={form.control}
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormControl>
-                                        <div className={"flex flex-col gap-y-4"}>
-                                            {
-                                                SIZES.map((size, idx) =>
-                                                    <div key={size + idx}
-                                                         className={"flex items-center justify-between"}>
-                                                        <Label htmlFor={size}
-                                                               className={"capitalize text-black/60"}>
-                                                            {size}
-                                                        </Label>
-                                                        <Checkbox
-                                                            id={size}
-                                                            className={"size-5"}
-                                                            checked={field.value.includes(size)}
-                                                            onCheckedChange={() => {
-                                                                fieldArrayOnChange(size, field.value, field.onChange)
-                                                            }}
-                                                        />
-                                                    </div>
-                                                )
-                                            }
-                                        </div>
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}/>
-                    </AccordionField>
-
-                    <Button
-                        type={"submit"}
-                        className={"mt-6 w-full rounded-3xl"}
-                    >
-                        Apply Filter
-                    </Button>
-                    <Button
-                        type={"button"}
-                        onClick={resetFilterHandler}
-                        className={"mt-4 w-full rounded-3xl bg-stone-500"}
-                    >
-                        Reset Filter
-                    </Button>
-                </form>
-            </Form>
-        </div>
-    );
+					<Button
+						type={"submit"}
+						className={"mt-6 w-full rounded-3xl"}>
+						Apply Filter
+					</Button>
+					<Button
+						type={"button"}
+						onClick={resetFilterHandler}
+						className={"mt-4 w-full rounded-3xl bg-stone-500"}>
+						Reset Filter
+					</Button>
+				</form>
+			</Form>
+		</div>
+	);
 };
 
 export default FilterForm;
