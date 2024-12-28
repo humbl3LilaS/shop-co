5f0747cb1c631a97d00dd1348148f818d9d6f628
humbl3LilaS <sabishinekobebe@gmail.com>
2024-12-28 21:25:30 +0630

    fix: the loading state /admin/dashboard/product/@chart is missing

---
+++ app/(admin)/admin/dashboard/overview/@chart/loading.tsx
@@ -0,0 +1,22 @@
+import Container from "@/components/admin/Container";
+import { getSalePeriod } from "@/feature/admin/overview/lib/util";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Chart = () => {
+    const salePeriod = getSalePeriod();
+    return (
+        <Container className={"col-span-3 row-span-2"}>
+            <div>
+                <h2 className={"mb-2 text-2xl font-semibold leading-none tracking-tight"}>
+                    Revenue
+                </h2>
+                <p text-sm text-muted-foreground>
+                    {salePeriod[0]} - {salePeriod[salePeriod.length - 1]} {new Date().getFullYear()}
+                </p>
+            </div>
+            <Skeleton className={"mt-4 flex-1 h-[340px]  w-full bg-gray-200"} />
+        </Container>
+    );
+};
+
+export default Chart;
--- feature/admin/overview/components/sale-chart.tsx
+++ feature/admin/overview/components/sale-chart.tsx
@@ -4,65 +4,68 @@ import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-    ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent
 } from "@/components/ui/chart";
 import { useQuery } from "@tanstack/react-query";
 import { getSalesPerMonth } from "@/feature/admin/overview/actions/get-sales-per-month";
 import { getSalePlaceHolder } from "@/feature/admin/overview/lib/get-sale-place-holder";
+import { getSalePeriod } from "@/feature/admin/overview/lib/util";
 
 const chartConfig = {
-    desktop: {
-        label: "Revenue",
-        color: "blue",
-    },
+  desktop: {
+    label: "Revenue",
+    color: "blue"
+  }
 } satisfies ChartConfig;
 
 export default function SaleChart() {
-    const { data } = useQuery({
-        queryKey: ["sale-data"],
-        queryFn: getSalesPerMonth,
-        staleTime: Infinity,
-        placeholderData: getSalePlaceHolder,
-    });
-
-    return (
-        <Card className={"rounded-xl p-4 h-full"}>
-            <CardHeader>
-                <CardTitle>Revenue</CardTitle>
-                <CardDescription>January - June 2024</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <ChartContainer config={chartConfig} className={"max-h-[260px] w-full"}>
-                    <LineChart
-                        accessibilityLayer
-                        data={data}
-                        margin={{
-                            left: 12,
-                            right: 12,
-                        }}
-                    >
-                        <CartesianGrid vertical={false} />
-                        <XAxis
-                            dataKey="month"
-                            tickLine={false}
-                            axisLine={false}
-                            tickMargin={8}
-                            tickFormatter={(value) => value.slice(0, 3)}
-                        />
-                        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-                        <Line
-                            dataKey="revenue"
-                            type="natural"
-                            stroke="var(--color-desktop)"
-                            strokeWidth={2}
-                            dot={false}
-                        />
-                    </LineChart>
-                </ChartContainer>
-            </CardContent>
-        </Card>
-    );
+  const { data } = useQuery({
+    queryKey: ["sale-data"],
+    queryFn: getSalesPerMonth,
+    staleTime: Infinity,
+    placeholderData: getSalePlaceHolder
+  });
+  const salePeriod = getSalePeriod();
+  return (
+    <Card className={"rounded-xl p-4 h-full"}>
+      <CardHeader>
+        <CardTitle>Revenue</CardTitle>
+        <CardDescription>
+          {salePeriod[0]} - {salePeriod[salePeriod.length - 1]} {new Date().getFullYear()}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig} className={"max-h-[260px] w-full"}>
+          <LineChart
+            accessibilityLayer
+            data={data}
+            margin={{
+              left: 12,
+              right: 12
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+            <Line
+              dataKey="revenue"
+              type="natural"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
 }
