ff071c88f23586480c92bef9a557cc4292437fa5
humbl3LilaS <sabishinekobebe@gmail.com>
2024-12-28 20:35:20 +0630

    refactor: data table into seperate component for dry purpose

--- feature/admin/overview/components/top-products.tsx
+++ feature/admin/overview/components/top-products.tsx
@@ -1,9 +1,8 @@
 "use client"
 import {columns} from "@/feature/admin/overview/components/table-columns";
-import {flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import {useQuery} from "@tanstack/react-query";
 import {getTopProducts} from "@/feature/admin/overview/actions/get-top-products";
+import DataTable from "@/components/share/data-table";
 
 const TopProducts = () => {
     const {data} = useQuery({
@@ -11,57 +10,8 @@ const TopProducts = () => {
         queryFn: getTopProducts,
         staleTime: Infinity,
     })
-
-    const table = useReactTable({
-        data: data ?? [],
-        columns: columns,
-        getCoreRowModel: getCoreRowModel(),
-    })
     return (
-        <div>
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                data-state={row.getIsSelected() && "selected"}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </div>
+      <DataTable columns={columns} data={data}/>
     );
 };
 
